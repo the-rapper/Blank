4ff308d442719e347573e00c775d798903687bf7
Gzrest <62439220+Tyrellghoul@users.noreply.github.com>
2024-11-16 11:11:08 -0500

    more main menu stuff

---
+++ Main menu/MainMenu.gd
@@ -0,0 +1,10 @@
+# MainMenu.gd
+extends Control
+
+# Nodes for different layers
+@onready var background_layer = $ParallaxBackground
+@onready var title = $Title
+@onready var menu_container = $MenuContainer
+@onready var animation_player = $AnimationPlayer
+
+	
