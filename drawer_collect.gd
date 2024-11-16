1d8fb28a8dab6ca2592010cc6921761fb4279f03
FightPancakes <143224772+FightPancakes@users.noreply.github.com>
2024-11-16 16:31:10 -0500

    Adjusting to remove auto popup issues

--- drawer_collect.gd
+++ drawer_collect.gd
@@ -16,7 +16,7 @@ func end():
 	drawer.end()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact") and hover:
+	if Input.is_action_just_released("interact") and hover:
 		drawer.end()
 	pass
 	
