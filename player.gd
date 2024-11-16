c02c7a33a99b1d38a05400b491ddc9f6bcc221a4
FightPancakes <143224772+FightPancakes@users.noreply.github.com>
2024-11-16 11:57:18 -0500

    Updating how the camera gets adjusted

--- player.gd
+++ player.gd
@@ -11,28 +11,30 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	turnaround()
 	look_down()
-	pass
 	
+
+var turnList = []
 var turn_deg = 0.0
 func turnaround() -> void:
 	if Input.is_action_just_pressed("look_left"):
 		turn_deg += 90
 	elif Input.is_action_just_pressed("look_right"):
 		turn_deg -= 90
-	var tween = get_tree().create_tween()
+	else: return
+	var tween = get_tree().create_tween().set_parallel(false)
 	tween.tween_property(self, "rotation_degrees", Vector3(0,turn_deg,0), 0.1)
+	turnList.append(Vector3(0,turn_deg,0))
 	pass
+	
+	
 @export var look_trigger = 0.7 # How far down the mouse needs to be to turn the mouse down
 @export var down_tilt_deg = 10 # The degrees to tilt the cam down
 func look_down() -> void:
 	
 	var mouse_low = get_viewport().get_mouse_position().y / get_viewport().size.y  # number from 0-1 that describes how low the mouse is
 	if look_trigger <= mouse_low and (int(turn_deg) % 360 == 0):
-		var tween = get_tree().create_tween()
+		var tween = get_tree().create_tween().set_parallel(false)
 		tween.tween_property(self, "rotation_degrees", Vector3(-down_tilt_deg,turn_deg + 20,0), 0.1)
-	elif rotation_degrees.x > 0:
-		var tween = get_tree().create_tween()
+	elif rotation_degrees.x < 0:
+		var tween = get_tree().create_tween().set_parallel(false)
 		tween.tween_property(self, "rotation_degrees", Vector3(0,turn_deg,0), 0.1)
-	
-		
-	
