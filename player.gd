1de7c35c16313b0675c5379f24db090661abd1ce
Cesar Ramos <97855377+livingcesium@users.noreply.github.com>
2024-11-16 18:59:52 -0500

    w pops

--- player.gd
+++ player.gd
@@ -5,6 +5,7 @@ extends Node3D
 func _ready() -> void:
 	Globals.can_interact = true
 	Globals.time_flowing = true
+	print(position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
