5118d973e49aeaa1f974cedd2e9af660ff787630
FightPancakes <143224772+FightPancakes@users.noreply.github.com>
2024-11-16 19:22:13 -0500

    Revert "win and lose conditions"
    This reverts commit 01ea7e6752ec2a6799c68637161695b4e9088c8f.

--- Globals.gd
+++ Globals.gd
@@ -2,14 +2,13 @@ extends Node
 var can_interact = false
 var time_flowing = false
 
-var participation = 10
+var participation = 100
 var is_camera_on = true
 var is_mic_on = true
-var is_participating = false # negative to lose participation over time, positive to gain participation, 0 for transition
+var is_participating = true # negative to lose participation over time, positive to gain participation, 0 for transition
 var since_participated = 0
 
 var call_time = 0
-const END_TIME = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,15 +28,9 @@ func _process(delta: float) -> void:
 		since_participated += delta
 		if since_participated > 10:
 			participation -= delta
-			if (participation < 0):
-				get_tree().change_scene_to_file("res://DisconnectScreen.tscn")
 	# call time tracker
 	if time_flowing:
 		call_time += delta
-		if call_time > END_TIME:
-			get_tree().change_scene_to_file("res://WinScreen.tscn")
-			#get_tree().create_timer(3.0).timeout
-			#get_tree().change_scene_to_file("res://Main menu/MainMenu.tscn")
 	pass
 
 func turn_camera_on() -> void:
