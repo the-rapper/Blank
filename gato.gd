b9c472699a406b1a43173af5d8411d29fffea8fc
Cesar Ramos <97855377+livingcesium@users.noreply.github.com>
2024-11-16 21:03:58 -0500

    More stuff

--- gato.gd
+++ gato.gd
@@ -1,11 +1,16 @@
 extends Node3D
+@export var calmness_min = -5
+@export var timeout = 15
+@export var odds = 0.3
+@onready var timer = get_tree().create_timer(timeout)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
+func try_and_retry() -> void:
+	return
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
